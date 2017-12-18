376ac3e5af0cc8e7b6ba306960f4c3a9a8c485a9
zxfjd3g <258147149@qq.com>
2017-12-18 09:26:00 +0800

    init app

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import counter from './components/counter.vue'
+
+new Vue({
+  el: '#app',
+  render: h => h(counter),  // h: 根据组件生成对应标签的函数
+  // components: {counter},
+  // template: '<counter />'
+ /* render: function (createElement) {
+    return createElement(counter)
+  }*/
+})
